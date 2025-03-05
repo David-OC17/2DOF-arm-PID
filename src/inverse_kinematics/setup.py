09804a327bcdca7d91b441f73ebb0664f391b8f2
frediromo <frediromo@gmail.com>
2025-03-05 13:34:29 -0600

    fix: typo in console scripts

--- src/inverse_kinematics/setup.py
+++ src/inverse_kinematics/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'inverse_kinematics_server = inverse_kinematics_server.main'
+            'inverse_kinematics_server = inverse_kinematics.inverse_kinematics_server:main'
         ],
     },
 )
